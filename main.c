4814a5fac2916be3a6697fd7a9fab9180f1c4f0f
Micke2310 <maikyarachan@gmail.com>
2024-04-23 02:10:49 +0000

    shell-done

--- main.c
+++ main.c
@@ -11,5 +11,11 @@ int main(){
 		printf("shell-done$ ");/* este es el texto a imprimir en pantalla como "PROMP"*/
 		scanf(" %99[^\n]", comando);/* esta funcion lee el comando x teclado, un maximo de 99 caracteres (cualquier)*/
 		                           /*con la excepcion del caracter nulo "\n" y lo guarda en la variable "comando"*/
-		if(!strcmp("exit", comando)){
-			break;}
+
+		if(!strcmp("exit", comando)){ /*esto es igual a (strcmp("exit", comando) == 0), pero , como en C 0 es falso,*/
+		      break;}		/*y esta funcion,si se cumple que ambas cadenas son iguales, arroja un 0 como "true"*/
+					/*entonces invierte el resultado de 0 con "!" y para el verdadero arrojara 0 pero,*/
+					/*al invertir la salida sera 1, porque para C 1 es "true". en cambio al igualarlo a 0*/
+					/*le decimos a C que para esta funcion, 0 sera el "verdadero" y uno el "falso". */
+					/*si strcmp = 0 ,(0 es "true", ambas son iguales), salir del loop con el "break".*/
+
